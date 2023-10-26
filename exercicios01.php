45f3f08b3aed516f7edbdddf432fcbf400b61967
GIUSEPPE-ESTUDIANTE <giusepe002@gmail.com>
2023-10-26 17:27:05 -0300

    exercicios-01

---
+++ exercicios01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 1</title>
+</head>
+<body>
+    <h1>exercicio 1 de php</h1>
+    <hr>
+    <?php
+    $chapolin =  "chapolin colorado";
+    $idade = 25;
+    
+    
+?>
+
+<p> <i> <?=$chapolin?> </i> possui <b><?=$idade?>  anos</b>.</p>
+
+
+</body>
+</html>
