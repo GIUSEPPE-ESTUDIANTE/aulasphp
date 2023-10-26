34274958550459f93c613ce1cdaf6eb4b8952361
GIUSEPPE-ESTUDIANTE <giusepe002@gmail.com>
2023-10-26 15:30:03 -0300

    php

---
+++ 01-sintaxe-geral.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+
+    <!--  delimitadores -->
+    <?php
+    echo "<i>Ola PHP!</i>";
+    echo 2023;
+    ?>
+
+    
+</body>
+</html>
